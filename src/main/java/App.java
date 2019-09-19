66bec544367241d425b205da5f9d88026525a2de
tuyetngo1 <tuyetngo136@gmail.com>
2019-09-19 16:24:44 +0100

    editdelete done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,8 +4,11 @@ import java.util.Map;
 
 import dao.Sql2oToDoDao;
 import models.ToDo;
+import org.slf4j.Logger;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import static spark.Spark.*;
 
 public class App {
@@ -22,14 +25,66 @@ public class App {
             return new ModelAndView(model, "index.vtl");
         }, new VelocityTemplateEngine());
 
-        get("/todos/:id", (request,response) -> {
+//        get("/todos/:id", (request,response) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            int id = Integer.parseInt(request.params("id"));
+//            System.out.println(id);
+//            ToDo todo1 = todoDao.find(id);
+//            System.out.println(todo1);
+//            model.put("todo",todo1);
+//            return new ModelAndView(model, "templates/todofind.vtl");
+//        }, new VelocityTemplateEngine());
+
+        get("/todos/new", (request,response)->{
             Map<String, Object> model = new HashMap<>();
-          int id = Integer.parseInt(request.params("id"));
-            System.out.println(id);
-            ToDo todo1 = todoDao.find(id);
-            System.out.println(todo1);
-            model.put("todo",todo1);
-            return new ModelAndView(model, "templates/todofind.vtl");
+            return new ModelAndView(model, "templates/todoadd.vtl");
         }, new VelocityTemplateEngine());
+
+        post("/todos/new", (request,response) -> {
+            String newToDo = request.queryParams("newtodo");
+            ToDo toDo = new ToDo(newToDo, 1);
+            todoDao.add(toDo);
+            response.redirect("/");
+            return null;
+        });
+
+        post("/todos/:id/delete",(request,response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int id = Integer.parseInt(request.params(":id"));
+            todoDao.delete(id);
+            response.redirect("/");
+            return null;
+        });
+
+        post("/todos/:id/complete", (request,response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int id = Integer.parseInt(request.params(":id"));
+            Boolean iscomplete = true;
+            todoDao.complete(id,iscomplete);
+//            model.put("complete",)
+            response.redirect("/");
+            return null;
+        });
+
+        get("/todos/:id/edit", (request,response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int id = Integer.parseInt(request.params(":id"));
+            ToDo todo = todoDao.find(id);
+            model.put("todo", todo);
+            System.out.println("********");
+            return new ModelAndView(model, "templates/edit.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/todos/:id/edit", (request,response) -> {
+            int id = Integer.parseInt(request.params(":id"));
+
+            System.out.println("****");
+            System.out.println(id);
+            String newContent = request.queryParams("newcontent");
+            todoDao.edit(id, newContent);
+            response.redirect("/");
+            return null;
+        });
     }
 }
+
